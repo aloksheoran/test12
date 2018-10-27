30a30c03cbdc44c2e49905524fe2755d234e0440
A <A@DESKTOP-RAAS93F>
2018-10-27 19:55:15 +0530

    this ran but nullpointer exception in mail
    xception in thread "main" java.lang.NullPointerException
    	at guru99project.guru99project.printtitlepage(guru99project.java:25)
    	at guru99project.Main_method.main(Main_method.java:10)

--- EdurekaProject/src/guru99project/guru99project.java
+++ EdurekaProject/src/guru99project/guru99project.java
@@ -1,24 +1,40 @@
 package guru99project;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class guru99project {
 	
-	ChromeDriver driver;
+ public WebDriver driver;
 		
 	public void invokechrome() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\A\\eclipse-workspace\\libs\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		System.setProperty("WebDriver.chrome.driver", "C://Users//A//eclipse-workspace//libs//chromedriver.exe");
 		Dimension dim = new Dimension(640,480);
 		driver.manage().window().setSize(dim);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.get("http://demo.guru99.com/v4");
+		driver.get("http://demo.guru99.com/v4");			
+	}
+	
+	public void printtitlepage() {
+		
+		System.out.println(driver.getTitle());		
+	}
+	
+	public void login (String uname, String pwd) {
+		WebElement usid = driver.findElement(By.name("uid"));		
+		usid.sendKeys(uname);
+		
+	 //   driver.findElement(By.name("uid")).sendKeys(uname);
+		driver.findElement(By.name("password")).sendKeys(pwd);
+		driver.findElement(By.name("btnLogin")).click();
+		
 		
 		
 	}
-	
 
 }
