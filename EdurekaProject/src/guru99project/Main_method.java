1683bd312e858b3ada1fcd1da61c2e617b1b8db8
A <A@DESKTOP-RAAS93F>
2018-10-27 20:52:56 +0530

    null pointer exception

---
+++ EdurekaProject/src/guru99project/Main_method.java
@@ -0,0 +1,15 @@
+package guru99project;
+
+public class Main_method {
+
+	public static void main(String[] args) {
+		
+		guru99project gp = new guru99project();
+		
+		gp.invokechrome();
+		gp.printtitlepage();
+		gp.login("mnr160177", "hynydej");
+
+	}
+
+}
