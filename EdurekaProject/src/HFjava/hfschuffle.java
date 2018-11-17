69dc397cef7ef6e307605919e9575f8c1ec39425
A <A@DESKTOP-RAAS93F>
2018-11-17 12:31:14 +0530

    aa

---
+++ EdurekaProject/src/HFjava/hfschuffle.java
@@ -0,0 +1,38 @@
+package HFjava;
+
+public class hfschuffle {
+
+	public static void main(String[] args) {
+        
+       int x = 3;
+       while (x>0) {
+    	   
+    	   if (x>2) {
+    		   
+    		   System.out.print("a");
+    		   
+    		   System.out.print("-");
+    	  }
+    	  
+    	   x=x-1; 
+       
+       
+       if (x==2) {
+    	   
+    	   System.out.print("b c");
+       }
+       x=x-1;
+		
+       if (x==1)  {
+    	   System.out.print("-");
+    	   
+    	   System.out.println("d");
+    	}
+		
+      
+	}
+
+}
+
+}
+
