9d24ea388d8d249494a3cfc5feb4f6b7da110576
A <A@DESKTOP-RAAS93F>
2018-11-19 00:20:04 +0530

    aa

--- EdurekaProject/src/HFjava/testarray.java
+++ EdurekaProject/src/HFjava/testarray.java
@@ -3,16 +3,32 @@ package HFjava;
 public class testarray {
 
 	public static void main(String[] args) {
-
-          int y = 0;
+          
+		  int[] index = new int[4];
+		  
+	       index[0] = 1;
+	       index[1] = 3;
+	       index[2] = 0;
+	       index[3] = 2;
+    
           String[] islands = new String[4];
-          int[] index = new int[4];
+       
           islands[0] = "Bermuda";
-          islands[0] = "Fiji";
-          islands[0] = "Azorea";
-          islands[0] = "Cozumel";
+          islands[1] = "Fiji";
+          islands[2] = "Azorea";
+          islands[3] = "Cozumel";
           
-          
+          int y = 0;
+   
+         int ref;
+    
+        while (y<4) {
+            ref = index[y];
+            System.out.print("island = ");
+        	System.out.println(islands[ref]);	
+        	y = y+1;
+        }
+        	
           
           
           
